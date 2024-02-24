19dd17b1180712d0f2a17c9a6f6cdbe756cb4b04
igor-devloper <wagnerigor9@gmail.com>
2024-02-24 11:12:09 -0300

    add links in button

--- src/app.tsx
+++ src/app.tsx
@@ -9,10 +9,12 @@ import { Footer } from './components/footer'
 export function App() {
   return (
     <main className='h-screen flex flex-col items-center bg-gray-950'>
+      <div className='z-50 flex flex-col items-center justify-center'>
+        <Head />
+        <Links />
+        <Footer />
+      </div>
       <StarsCanvas />
-      <Head />
-      <Links />
-      <Footer/>
     </main>
   )
 }
--- src/components/links.tsx
+++ src/components/links.tsx
@@ -15,52 +15,44 @@ export function Links() {
       className="flex flex-col justify-center w-full space-y-10 mt-5 max-w-[500px]"
     >
 
-      <motion.div variants={slideInFromTop(4)}>
-        <Button onClick={() => {
-          track('Click', {
-            message: 'Um usuario clicou no Link do instagram'
-          })
-        }}  className="flex items-center gap-2 p-6 w-full" variant='card'>
-          <Instagram />
-          Instagram
-        </Button>
-      </motion.div>
+      <a href="https://www.instagram.com/stories/idomktdigital" target="_blank">
+        <motion.div variants={slideInFromTop(4)}>
+          <Button type="button" onClick={() => { track('Click', { message: 'Um usuario clicou no Link do instagram' }) }} className="flex items-center gap-2 p-6 w-full" variant='card'>
+            <Instagram />
+            Instagram
+          </Button>
+        </motion.div>
+      </a>
+
+      <a href="https://idomktdigital.vercel.app/" target="_blank">
+        <motion.div variants={slideInFromTop(4.2)}>
+          <Button type="button" onClick={() => { track('Click', { message: 'Um usuario clicou no Link do Site' }) }} className="flex items-center gap-2 p-6 w-full" variant='card'>
+            <Blocks />
+            Site
+          </Button>
+        </motion.div>
+      </a>
 
 
+      <a href="#">
+        <motion.div variants={slideInFromTop(4.2)}>
+          <Button type="button" onClick={() => { track('Click', { message: 'Um usuario clicou no Link do Site' }) }} className="flex items-center gap-2 p-6 w-full" variant='card'>
+            <Linkedin />
+            Linkedin
+          </Button>
+        </motion.div>
+      </a>
 
-      <motion.div variants={slideInFromTop(4.2)}>
-        <Button onClick={() => {
-          track('Click', {
-            message: 'Um usuario clicou no Link do Site'
-          })
-        }} className="flex items-center gap-2 p-6 w-full" variant='card'>
-          <Blocks />
-          Site
-        </Button>
-      </motion.div>
-
-
-      <motion.div variants={slideInFromTop(4.4)}>
-        <Button onClick={() => {
-          track('Click', {
-            message: 'Um usuario clicou no Link do Linkedin'
-          })
-        }} className="flex items-center gap-2 p-6 w-full" variant='card'>
-          <Linkedin />
-          Linkedin
-        </Button>
-      </motion.div>
-
-      <motion.div variants={slideInFromTop(4.6)}>
-        <Button onClick={() => {
-          track('Click', {
-            message: 'Um usuario clicou no Link do Facebook'
-          })
-        }} className="flex items-center gap-2 p-6 w-full" variant='card'>
-          < Facebook />
-          Facebook
-        </Button>
-      </motion.div>
+      <a href="#">
+        <motion.div variants={slideInFromTop(4.6)}>
+          <Button type="button" onClick={() => {
+            track('Click', { message: 'Um usuario clicou no Link do Facebook' })
+          }} className="flex items-center gap-2 p-6 w-full" variant='card'>
+            < Facebook />
+            Facebook
+          </Button>
+        </motion.div>
+      </a>
     </motion.div>
 
 
